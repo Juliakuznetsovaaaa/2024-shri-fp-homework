3ef035b689cfaca1bf1ec7d46a6ad8486d786f7c
Juliakuznetsovaaaa <99537178+Juliakuznetsovaaaa@users.noreply.github.com>
2024-07-27 18:33:29 +0300

    second task done

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -15,37 +15,75 @@
  * Ответ будет приходить в поле {result}
  */
  import Api from '../tools/api';
-
+ import * as ramda from 'ramda'
+ import {pipe, lt, gt} from 'ramda'
  const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+ //валидация
+
+ const isLessThan10 = pipe(ramda.split(''), ramda.length, gt(10));
+ const isMoreThan2 = pipe(ramda.split(''), ramda.length, lt(2));
+ const isPositiveNum  = pipe(ramda.startsWith('-'), ramda.not)
+ const isNumber = pipe(ramda.add(0), ramda.type, ramda.equals('Number'));
+ const validate = ramda.allPass([isLessThan10, isMoreThan2, isPositiveNum, isNumber]);
+
+ //преобразования
+ const toNumber = ramda.pipe(parseFloat, Math.round);
+ const square = (value) => value ** 2;
+ const modulo3 = (num) => ramda.modulo(num, 3)
+
+ const params = (number) => ({
+   from: 10,
+   to: 2,
+   number,
+ });
+
+ const getNumber = (number) =>
+    api.get('https://api.tech/numbers/base', params(number))
+     
+  
+  const getAnimal = (id) =>
+    api.get(`https://animals.tech/${id}`, id)
+
 
  const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
-
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
-
-     wait(2500).then(() => {
-         writeLog('SecondLog')
-
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
-
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+     
+    const tapWriteLog =(val) => ramda.tap(writeLog, val); 
+    const handleValidationError = () => handleError('ValidationError');
+   
+    const processGetAnimal = ramda.pipe(
+       tapWriteLog,
+       ramda.length,
+       tapWriteLog,
+       square,
+       tapWriteLog,
+       modulo3,
+       tapWriteLog,
+       getAnimal, 
+       ramda.otherwise(handleError),
+       ramda.andThen(
+           ramda.when(ramda.has('result'),
+           pipe(ramda.prop('result'), handleSuccess)
+           )
+       )
+   )
+   
+    const processRequests = pipe (
+       toNumber,
+       getNumber,
+       ramda.otherwise(handleError),
+       ramda.andThen(
+           ramda.when(ramda.has('result'),
+           pipe(ramda.prop('result'), processGetAnimal)
+           )
+       )
+   )
+   
+    const processValue = ramda.pipe(tapWriteLog,
+      ramda.ifElse(validate, processRequests, handleValidationError)
+      );
+   
+    processValue(value);
+}
 
 export default processSequence;
